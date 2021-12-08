6cb493f5f440b2023471125a02685301e326ca0b
devdesperado <95681673+devdesperado@users.noreply.github.com>
2021-12-08 18:11:26 +0900

    1
    삼각형

---
+++ seokhyun/triangle.java
@@ -0,0 +1,17 @@
+package seokhyun;
+import java.util.Scanner;
+public class triangle {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("삼각형의 넓이를 구합니다");
+
+        System.out.print("밑변 :");
+        double width = stdIn.nextDouble();
+
+        System.out.print("높이: ");
+        double height = stdIn.nextDouble();
+
+        System.out.println("넓이는 " + (width * height / 2) + "입니다");
+    }
+}
